15d944370f1be1d7557691ee13a1c735ae3f91d0
Nguyên Nguyễn Đặng <teknix_nguyendang@Nguyens-MacBook-Air.local>
2025-04-18 09:50:48 +0700

    new version

--- src/components/ui/DepositFunction/index.tsx
+++ src/components/ui/DepositFunction/index.tsx
@@ -1,3 +1,4 @@
+"use client";
 import { forwardRef, useImperativeHandle, useRef, useState } from "react";
 import { GeneralProps } from "../../../types/ui";
 import DrawerComponent, { DrawerComponentRef } from "../DrawerComponent";
