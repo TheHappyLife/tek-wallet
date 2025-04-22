6e1ed9fb40f73a2f461009377a65c580c8881317
Nguyên Nguyễn Đặng <teknix_nguyendang@Nguyens-MacBook-Air.local>
2025-04-22 11:04:33 +0700

    new version

--- src/services/axios/get-config-tokens-list/index.ts
+++ src/services/axios/get-config-tokens-list/index.ts
@@ -4,14 +4,14 @@ export type TransactionType = "deposit" | "withdrawn" | "transfer-internal";
 export interface GetConfigTokenListQuery {
   page?: number;
   take?: number;
-  transaction_type: TransactionType;
+  transactionType: TransactionType;
 }
 
 const getConfigTokenList = async <T>(
   query?: GetConfigTokenListQuery
 ): Promise<T> => {
   const response = await userClientRequest.get("/platform-config/list-tokens", {
-    params: query,
+    params: { ...query, transaction_type: query?.transactionType },
   });
 
   console.warn("🚀 ~ getConfigTokenList response:", response);
