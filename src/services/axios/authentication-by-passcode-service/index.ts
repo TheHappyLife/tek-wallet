557c3a53d68bb007a2818acc6a9d059cbaeb4260
Nguyên Nguyễn Đặng <teknix_nguyendang@Nguyens-MacBook-Air.local>
2025-04-23 16:51:01 +0700

    new version

--- src/services/axios/authentication-by-passcode-service/index.ts
+++ src/services/axios/authentication-by-passcode-service/index.ts
@@ -7,11 +7,15 @@ import userClientRequest from "../clients/userClientRequest";
 export default async function authenticationByPasscode(
   body: AuthenticationByPasscodeBody
 ): Promise<AuthenticationByPasscodeResponse> {
-  const response = await userClientRequest.post("/auth/passcode", body, {
-    headers: {
-      "c-payload-signature": "text",
-    },
-  });
+  const response = await userClientRequest.post(
+    "/wallets/verify-passcode",
+    body,
+    {
+      headers: {
+        "c-payload-signature": "text",
+      },
+    }
+  );
   console.warn("🚀 ~ response authenticationByPasscode:", response);
 
   return response?.data;
