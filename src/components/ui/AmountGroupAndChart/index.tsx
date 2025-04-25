e4efe06ad461110e5aec097b68588796477c8b48
Nguyên Nguyễn Đặng <teknix_nguyendang@Nguyens-MacBook-Air.local>
2025-04-25 08:28:45 +0700

    new version

--- src/components/ui/AmountGroupAndChart/index.tsx
+++ src/components/ui/AmountGroupAndChart/index.tsx
@@ -130,12 +130,12 @@ const AmountGroupAndChart = (props: AmountGroupAndChartProps) => {
             variants={variants}
             transition={{
               opacity: {
-                duration: expandAreaChart ? 0.5 : 0.25,
+                duration: expandAreaChart ? 0.5 : 0.1,
                 ease: "linear",
                 delay: expandAreaChart ? 0.5 : 0,
               },
               left: {
-                duration: expandAreaChart ? 0.5 : 0.25,
+                duration: expandAreaChart ? 0.5 : 0.2,
                 ease: "linear",
                 delay: expandAreaChart ? 0.3 : 0,
               },
