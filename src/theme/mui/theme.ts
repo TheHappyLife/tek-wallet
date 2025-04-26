ef7688ceebbc753d615fd951de420e0af6c02e18
Nguyên Nguyễn Đặng <teknix_nguyendang@Nguyens-MacBook-Air.local>
2025-04-26 08:23:47 +0700

    new version

--- src/theme/mui/theme.ts
+++ src/theme/mui/theme.ts
@@ -125,7 +125,7 @@ const theBorderRadius = {
 const shadows = [
   "none",
   "inset 0px 0px 50px 8px rgba(255, 255, 255, 0.1)", // 0
-  "0px 3px 6px rgba(255, 255, 255, 0.1)", // 1
+  "0px 0px 50px 8px rgba(255, 255, 255, 0.1)", // 1
   "0px 5px 15px rgba(255, 255, 255, 0.1)", // 2
   "0px 5px 15px rgba(255, 255, 255, 0.2)", // 3
   "0px 6px 24px rgba(255, 255, 255, 0.1)", // 4
