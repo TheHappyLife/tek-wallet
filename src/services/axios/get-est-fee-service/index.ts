a986efd66666caeb36e142aeeedc7aaf3c2a75b7
Nguyên Nguyễn Đặng <teknix_nguyendang@Nguyens-MacBook-Air.local>
2025-04-28 17:30:22 +0700

    new version

--- src/services/axios/get-est-fee-service/index.ts
+++ src/services/axios/get-est-fee-service/index.ts
@@ -5,12 +5,9 @@ import { GetEstimateFeeServiceQuery } from "./type";
 export default async function getEstimateFeeService(
   query: GetEstimateFeeServiceQuery
 ): Promise<GetEstimateFeeServiceResponse> {
-  const response = await userClientRequest.get(
-    `/fee/calculate?amount=${query.amount}&transaction_type=${query.transaction_type}&currency=${query.currency}`,
-    {
-      params: query,
-    }
-  );
+  const response = await userClientRequest.get(`/fee/calculate`, {
+    params: query,
+  });
   console.warn("🚀 ~ getEstimateFeeService response:", response);
 
   return response.data;
