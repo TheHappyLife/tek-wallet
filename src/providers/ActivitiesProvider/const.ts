e1d4af1f0098dc108b13f3308bedd7f77b34a989
Nguyên Nguyễn Đặng <teknix_nguyendang@Nguyens-MacBook-Air.local>
2025-05-07 14:09:50 +0700

    new version

--- src/providers/ActivitiesProvider/const.ts
+++ src/providers/ActivitiesProvider/const.ts
@@ -1,7 +1,13 @@
-import { TransactionSlug } from "../../services/axios/get-activities-service/type";
+import { TransactionSlug, TransactionType } from "../../services/axios/get-activities-service/type";
 
 export const ACTIVITIES_PAGE_SIZE = 10;
-export const ACTIVITIES_TYPE_ALL = {
+export const ACTIVITIES_TYPE_ALL: TransactionType = {
+  id: -1,
+  status: "all",
   name: "All",
   slug: TransactionSlug.All,
+  description: null,
+  icon: "",
+  link: "",
+  icon_svg: "",
 };
